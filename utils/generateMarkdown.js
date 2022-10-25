8e3092994df3bc54835656c1c8f1b620577bbdc8
KC <kevinchen@kevins-air.myfiosgateway.com>
2022-10-25 10:21:36 -0400

    badges added in utils

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,41 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return `![badge](https://img.shields.io/badge/license-${license}-green)`;
+  } 
+    else {
+    return ' ';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return `{[License](# License)}`;
+    } else {
+      return ' ';
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license != "None") {
+    return ` # License ${license}`
+    } else {
+      return ``;
+    }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
 # Project Title
 ${data.title}
+
+${renderLicenseBadge(data.license)}
 
 # Description
 ${data.description}
@@ -33,9 +54,11 @@ ${data.installation}
 
 # Usage
 ${data.usage}
+${renderLicenseSection}
 
 # License 
 ${data.license}
+${renderLicenseLink}
 * As this license list was not comprehensive, if you need another license, use the contact information below to ask for license to be added. 
 
 # Contributing 
